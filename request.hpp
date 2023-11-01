bde63d8e90b71b8ed2d86c1b751d6ee527fa4aaa
tttyyzzz <tohyizhi9@gmail.com>
2023-11-01 18:52:13 +0800

    Add functions to check if the cache set is full and the tag of the LRU Cacheline

--- request.hpp
+++ request.hpp
@@ -4,7 +4,7 @@ class Request {
   private:
     bool done = false;
     unsigned int countdown = 0;
-
+    unsigned int address = Null;
   public:
     bool isDone() { return done; };
     void decrement() { countdown--; }
