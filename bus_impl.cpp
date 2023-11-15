7831a843f259e8abe45c749d3508ab3e94ad6f17
Adam Oh <adamohzhihong@gmail.com>
2023-11-15 17:36:16 +0800

    fix bus impl

--- bus_impl.cpp
+++ bus_impl.cpp
@@ -50,28 +50,51 @@ void BusImpl::processRequest(shared_ptr<Request> request) {
         }
         State newState = isShared ? S : E;
         processors[request->pid]->setState(request->address, newState);
-
-        // cache->setCacheLineState(address, newState);
+        if (isShared) {
+            // flush, this case is 2n + 100 + 2n
+            request->countdown = 2; // TODO: GET BLOCK SIZE HERE
+        } else {
+            // don't need to flush, just load from memory, this case is 100 + 2n
+            request->countdown = 100;
+            memRequests.push_back(request);
+            currReq = nullptr;
+        }
     }
 }
 
 void BusImpl::executeCycle() {
-    if (currReq == nullptr && busQueue.empty()) {
+    if (currReq == nullptr && busQueue.empty() && memRequests.empty()) {
         return;
     }
+
+    // pop if no current request
     if (currReq == nullptr && !busQueue.empty()) {
         currReq = busQueue.front();
         busQueue.pop();
         processRequest(currReq);
     }
-    currReq->countdown--;
-    if (currReq->countdown == 0) {
-        if (currReq->isModified) {
-            memRequests.push_back(currReq);
-        } else {
-            currReq->done = true;
+
+    // handle current request if there is one
+    if (currReq != nullptr) {
+        currReq->countdown--;
+        if (currReq->countdown == 0) {
+            if (currReq->isModified) {
+                memRequests.push_back(currReq);
+            } else {
+                currReq->done = true;
+            }
+            currReq = nullptr;
+            return;
         }
-        currReq = nullptr;
-        return;
+    }
+
+    // decrement memory accesses
+    for (auto req : memRequests) {
+        req->countdown--;
+        if (req->countdown == 0) {
+            req->countdown = 2;
+            // this last 2n request is still tagged to the original processor in the bus map
+            busQueue.push(req);
+        }
     }
 }
