6ff724d1d750bb969fda2280c7c11bfd82c68d8d
Adam Oh <adamohzhihong@gmail.com>
2023-11-19 01:25:53 +0800

    refactor and fix logic for processBusRd

--- bus_impl.cpp
+++ bus_impl.cpp
@@ -44,20 +44,32 @@ void BusImpl::issueInvalidation(unsigned int pid) {
     }
 }
 
+/*
+ * assume that processing a bus request happens within one cycle
+ */
 void BusImpl::processBusRd(shared_ptr<Request> request) {
-    bool isShared = false;
+    // check if any other processor has it in M state
+    // sanity check : if there exists the block in M state then they need to flush it
+    // otherwise, everyone who has the block MUST match memory. so 100 + 2n
+    bool isModified = false; // used to decide where the request goes
+    bool isShared = false;   // used to decide the new state of the current processor
     for (auto p : this->processors) {
-        isShared |= p->onBusRd(request->address);
+        State pState = p->getState(request->address);
+        isModified |= (pState == M);
+        isShared |= (pState == M || pState == E || pState == S);
         p->setState(request->address, S);
     }
     State newState = isShared ? S : E;
     processors[request->pid]->setState(request->address, newState);
-    if (isShared) {
+    if (isModified) {
         // flush, this case is 2n + 100 + 2n
-        request->countdown = 2; // TODO: GET BLOCK SIZE HERE
+        // this request is spending 2n on the bus, and then going to memory
+        request->countdown = 2 * wordsPerBlock;
+        request->isToMemOrCache = true;
     } else {
-        // don't need to flush, just load from memory, this case is 100 + 2n
+        // load from memory, this case is 100 + 2n
         request->countdown = 100;
+        request->isToMemOrCache = false;
         memRequests.push_back(request);
         currReq = nullptr;
     }
@@ -89,7 +101,7 @@ void BusImpl::executeCycle() {
     if (currReq != nullptr) {
         currReq->countdown--;
         if (currReq->countdown == 0) {
-            if (currReq->isModified) {
+            if (currReq->isToMemOrCache) {
                 memRequests.push_back(currReq);
             } else {
                 currReq->done = true;
